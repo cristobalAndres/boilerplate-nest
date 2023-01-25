646303cdbde24a5fa850b74277314ee735f341cc
Ian Cardenas <icardenac@bancoripley.com>
2023-01-25 13:45:57 -0300

    simplify controller

--- src/summary/controllers/summary.controller.ts
+++ src/summary/controllers/summary.controller.ts
@@ -1,12 +1,10 @@
 import {
   Body,
   Controller,
-  Get,
   HttpCode,
   HttpException,
   HttpStatus,
   Logger,
-  Param,
   Post,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
@@ -25,16 +23,8 @@ export class SummaryController {
   @Post()
   @HttpCode(HttpStatus.OK)
   async findAll(@Body() body: RequestOptionsDTO): Promise<SummaryReponseDTO> {
-    try {
-      const summary = await this.summaryService.getSummary(body);
-      if (summary instanceof Array) return summary[0];
-      return summary;
-    } catch (error) {
-      this.logger.error(`Error getting accounts: ${error.message}`);
-      throw new HttpException(
-        { statusCode: 500, message: 'test message error' },
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    const summary = await this.summaryService.getSummary(body);
+    if (summary instanceof Array) return summary[0];
+    return summary;
   }
 }
