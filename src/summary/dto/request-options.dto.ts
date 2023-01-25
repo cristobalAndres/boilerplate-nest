bbe2a6d85adcbc0db65020b279d180332ac03cde
Ian Cardenas <icardenac@bancoripley.com>
2023-01-25 17:27:12 -0300

    add validations

--- src/summary/dto/request-options.dto.ts
+++ src/summary/dto/request-options.dto.ts
@@ -1,4 +1,10 @@
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
+
 export class RequestOptionsDTO {
+  @IsDateString()
+  @IsNotEmpty()
   processDate: Date;
+  @IsString()
+  @IsNotEmpty()
   userId: string;
 }
