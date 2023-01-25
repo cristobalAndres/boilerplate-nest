097d2d6527807b15b9410dfcbe7c2a694f081185
Ian Cardenas <icardenac@bancoripley.com>
2023-01-25 06:25:37 -0300

    remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SummaryModule } from './summary';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SummaryModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
