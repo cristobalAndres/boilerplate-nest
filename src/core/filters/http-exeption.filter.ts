5599a6fa61a5a508112d29814b48812704c33cee
Ian Cardenas <icardenac@bancoripley.com>
2023-01-25 17:57:21 -0300

    remove interceptor error and add filter error

---
+++ src/core/filters/http-exeption.filter.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExeptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const message = exception.message;
+    const description = exception.name;
+    const cause = exception.cause || exception;
+    response.status(status).json({
+      statusCode: status,
+      path: request.url,
+      message,
+      description,
+      cause,
+    });
+  }
+}
